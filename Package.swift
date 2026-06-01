926a934608c0ae4130505aa8dbd8cb82d42e4cdb
Philipp Walter <philippwalter@proton.me>
2026-06-01 18:24:40 +0200

    chore: prepare v0.5.13 package checksum

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let tag = "v0.5.12"
-let checksum = "e6068b6d3301af092a65a6f8f4571165b620655c88ba7e0ce3eb8c8dcc1b096e"
+let tag = "v0.5.13"
+let checksum = "5b0143f05a1f49c580b8442b43e18293c5a7af6e97071221ca4899877898a1ad"
 let url = "https://github.com/synonymdev/vss-rust-client-ffi/releases/download/\(tag)/VssRustClientFfi.xcframework.zip"
 
 let package = Package(
