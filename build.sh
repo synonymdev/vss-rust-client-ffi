78a20faebca8a4ba041450b111048d2e0f728db1
Philipp Walter <philippwalter@proton.me>
2026-06-02 20:15:29 +0200

    chore: remove Python bindings

--- build.sh
+++ build.sh
@@ -8,14 +8,11 @@ case "$1" in
   "android")
     ./build_android.sh
     ;;
-  "python")
-    ./build_python.sh
-    ;;
   "all")
-    ./build_ios.sh && ./build_android.sh && ./build_python.sh
+    ./build_ios.sh && ./build_android.sh
     ;;
   *)
-    echo "Usage: $0 {ios|android|python|all}"
+    echo "Usage: $0 {ios|android|all}"
     exit 1
     ;;
 esac
