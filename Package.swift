7fe6557b2b0c1c812a86fd728a993485174c4c85
Ovi Trif <ovitrif@proton.me>
2026-06-05 00:14:59 +0200

    chore: release vss 0.5.16

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let tag = "v0.5.14"
-let checksum = "a4c420eb50c00933e26b9fa48ca015015a2b889214e148a414ba087796cd2d14"
+let tag = "v0.5.16"
+let checksum = "e2d85c029c7be5dc52a8d268297b7ded864aa35435f0d042dfcc31352235d386"
 let url = "https://github.com/synonymdev/vss-rust-client-ffi/releases/download/\(tag)/VssRustClientFfi.xcframework.zip"
 
 let package = Package(
